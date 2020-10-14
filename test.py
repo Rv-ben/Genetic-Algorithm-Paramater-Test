1449c960d16d5c036b4f60a8a8aaa8d9ec5d4052
Rv-ben <Ruben.Bramasco@student.csulb.edu>
2020-10-13 18:44:24 -0700

    Added  a trails option

--- test.py
+++ test.py
@@ -9,14 +9,14 @@ import logging
 
 
 
-def computeProbFixed():
+def computeProbFixed(trials):
     PopData = {}
     #Test for fixed ns values [5,6,7]
     #Fixed probablity of no mutation to 1/10
     count1 = 0
     for ns in [4,5,6,7]:
         PopData[ns] = {2:0,4:0,6:0,8:0,10:0,12:0,14:0}
-        for j in range(10):
+        for j in range(trials):
             for p in range(2,16,2):
                 objects = [board(ns) for i in range(p)]
                 popTest = genetic(objects,ns,ns+1)
@@ -24,6 +24,7 @@ def computeProbFixed():
                 PopData[ns].update({p:PopData[ns][p] + popTest.getIterations()})
                 logging.info(str(count1)+" out of 180 Probablity Fixed Test")
                 count1 = count1 +1
+    
 
     logging.info(str(count1)+" out of 180 Probablity Fixed Test")
 
@@ -31,14 +32,14 @@ def computeProbFixed():
 
     file1.write(json.dumps(PopData))
 
-def computePopFixed():
+def computePopFixed(trials):
     #Test for fixed ns values [5,6,7]
     #Fixed population of 4
     ProbData = {}
     count2 = 0
     for ns in [4,5,6,7]:
         ProbData[ns] = {1:0,2:0,3:0,4:0,5:0,6:0,7:0}
-        for j in range(10):
+        for j in range(trials):
             for p in range(7):
                 objects = [board(ns) for i in range(8)]
                 probTest = genetic(objects,ns,ns+p)
@@ -56,8 +57,8 @@ def computePopFixed():
 format = "%(asctime)s: %(message)s"
 logging.basicConfig(format=format, level=logging.INFO,datefmt="%H:%M:%S")
 
-popFixed = threading.Thread(target=computePopFixed)
-probFixed = threading.Thread(target=computeProbFixed)
+popFixed = threading.Thread(target=computePopFixed,args=(100))
+probFixed = threading.Thread(target=computeProbFixed,args=(100))
 
 popFixed.start()
 probFixed.start()
